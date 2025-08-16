9082bcb06ef20feb30d09e2185215208f550a415
Peyruis <undera815@gmail.com>
2025-08-16 11:59:05 -0500

    Version_2

--- Ejm_1/Form1.Designer.cs
+++ Ejm_1/Form1.Designer.cs
@@ -28,21 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Numero1 = new System.Windows.Forms.Label();
+            this.Numero2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Numero1
+            // 
+            this.Numero1.AutoSize = true;
+            this.Numero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Numero1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Numero1.Location = new System.Drawing.Point(258, 187);
+            this.Numero1.Name = "Numero1";
+            this.Numero1.Size = new System.Drawing.Size(82, 20);
+            this.Numero1.TabIndex = 0;
+            this.Numero1.Text = "Numero 1";
+            // 
+            // Numero2
+            // 
+            this.Numero2.AutoSize = true;
+            this.Numero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Numero2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Numero2.Location = new System.Drawing.Point(258, 237);
+            this.Numero2.Name = "Numero2";
+            this.Numero2.Size = new System.Drawing.Size(82, 20);
+            this.Numero2.TabIndex = 1;
+            this.Numero2.Text = "Numero 2";
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.SkyBlue;
+            this.txt1.Location = new System.Drawing.Point(377, 189);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // txt2
+            // 
+            this.txt2.BackColor = System.Drawing.Color.SkyBlue;
+            this.txt2.Location = new System.Drawing.Point(377, 239);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(596, 182);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(596, 225);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(596, 273);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(377, 286);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(258, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Resultado";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1087, 659);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.Numero2);
+            this.Controls.Add(this.Numero1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.BackColorChanged += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Numero1;
+        private System.Windows.Forms.Label Numero2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Ejm_1/Form1.cs
+++ Ejm_1/Form1.cs
@@ -21,5 +21,46 @@ namespace Ejm_1
         {
 
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+
+            double n1, n2;
+            string resultado = "";
+
+            n1 = Double.Parse(txt1.Text);
+            n2 = Convert.ToDouble(txt2.Text);  
+
+
+
+            if (n1 > n2)
+            {
+                resultado = "Numero : " + n1 + " es mayor que " + n2;
+            } else if (n1 < n2)
+            {
+                resultado = "Numero :" + n2 + " es mayor que " + n1;
+            }else if(n1 == n2)
+            {
+                resultado = "Numero : " + n1 + " es igual a " + n2;
+            }
+            txtResultado.Text = resultado;
+            MessageBox.Show(resultado, "Evaluar 2 numeros",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txt1.Text = string.Empty;
+            txt2.Text = string.Empty;
+            txtResultado.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Dispose(); 
+        }
     }
 }
